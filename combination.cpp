f07d3f6ac0073f122ecd693dd9c7b0af383808aa
Haibo Shi <haiboshiaaa@gmail.com>
2015-06-19 23:19:01 +0800

    1118

---
+++ combination.cpp
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: combination.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri Jun 19 23:17:46 2015
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+
+int main(){
+
+
+    return 0;
+}
